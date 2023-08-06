b7054555f82d45ca0bc64c473608837262ecd4c6
mharshr2135 <141387860+mharshr2135@users.noreply.github.com>
2023-08-06 11:34:21 +0530

    Update ChatBox - Client1.py

--- ChatBox - Client1.py
+++ ChatBox - Client1.py
@@ -2,6 +2,7 @@ import socket
 
 c = socket.socket()
 c.connect(('localhost',9999))
-name = input("Enter your name")
-c.send(bytes(name,'utf-8'))
-print(c.recv(1024).decode())
+while True
+  mes = input()
+  c.send(bytes(mes,'utf-8'))
+  print(c.recv(1024).decode())
