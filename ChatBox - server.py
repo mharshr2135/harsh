a5e7290ca66e78e555c0da79314d7cc5b674e679
mharshr2135 <141387860+mharshr2135@users.noreply.github.com>
2023-08-06 11:42:17 +0530

    Update ChatBox - server.py

--- ChatBox - server.py
+++ ChatBox - server.py
@@ -1,14 +1,14 @@
 import socket
 
-s = socket.socket()
+s1 = socket.socket()
 print("socket created")
-s.bind(('localhost',9999))
-s.listen(3)
+s1.bind(('localhost',9999))
+s1.listen(3)
 
 while True:
-    c,addr = s.accept()
-    mes1 = c.recv(1024).decode()
-    print("Connected with", addr,mes1)
+    c1,addr1 = s1.accept()
+    mes1 = c1.recv(1024).decode()
+    print("Connected with", addr1,mes1)
 
-    c.send(bytes("welcome",'utf-8'))
+    #c.send(bytes("welcome",'utf-8'))
     c.close()
